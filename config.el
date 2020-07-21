7cb52575b330fcbb8b604d6df89d052a139fc41b
Sergey Mishin <sergei.a.mishin@gmail.com>
2020-07-21 23:18:28 +0300

    Use window-system to guess the OS

--- config.el
+++ config.el
@@ -59,3 +59,10 @@
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
 (turn-off-smartparens-mode)
+
+(defvar ns window-system)
+(when (string= ns "x")
+  (setq doom-font (font-spec :family "Terminus" :size 10 :weight 'semi-light)
+        ;; doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
+        doom-unicode-font (font-spec :family "Fira Code" :size 10)
+        doom-big-font (font-spec :family "Fira Code" :size 14)))
